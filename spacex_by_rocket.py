121752636f477ab6c42ca83c1cee1aec28261c90
Sergei Perfilev <sergei74ap@mail.ru>
2021-02-27 18:10:12 +0700

    Add files via upload

---
+++ spacex_by_rocket.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2005, 1, 1),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
+
+rocket_models = (
+    "falcon1", 
+    "falcon9", 
+    "falconheavy"
+)
+
+loads = [
+BashOperator(
+    task_id=f"get_data_{rocket}",
+    bash_command="python3 /root/airflow/dags/spacex/load_launches.py " \
+                 "-y {{ execution_date.year }} -o /var/data " \
+                 "-r {{ params.rocket }}", 
+    params={"rocket": rocket}, 
+    dag=dag) for rocket in rocket_models
+]
+
+prints = [
+BashOperator(
+    task_id=f"print_data_{rocket}", 
+    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+    params={"rocket": rocket},
+    dag=dag) for rocket in rocket_models
+]
+
+loads >> prints
